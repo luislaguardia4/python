76a350c3a3fefd13e7c0e1a78ba2ba8ba48f747e
Luis Laguardia <61608109+luislaguardia4@users.noreply.github.com>
2023-03-21 00:59:54 +0800

    Create computingAVG.py

---
+++ Array/computingAVG.py
@@ -0,0 +1,34 @@
+#avg
+print("LUIS GABRIEL H. LAGUARDIA \n ======================")
+e, f, g, h, i, j, k, l, m = "l", "a", "g", "u", "a", "r", "d", "i", "a"
+print (e)
+print (f)
+print (g)
+print (h)
+print (i)
+print (j)
+print (k)
+print (l)
+print (m)
+
+print ("")
+#same caller // three variable
+x = y = z = "white"
+print (x)
+print (y)
+print (z)
+
+print("")
+age = int(input("Enter your age: "))
+name = str(input("Enter your name: "))
+
+
+x = input("Enter your grade in 1st sem: ")
+y = input("Enter your grade in 2nd sem: ")
+z = input("Enter your grade in 3rd sem: ")
+sum = float(x) + float(y) + float(z)
+avg = (sum) / 3
+
+print ("hi," ,name, " you are " ,age,)
+print ("your average is: ",avg,)
+print ("National University")
