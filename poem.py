08d627030cbd4ff698089e01bf8a0420f8083846
Luis Laguardia <61608109+luislaguardia4@users.noreply.github.com>
2023-03-21 01:01:53 +0800

    Create poem.py

---
+++ poem.py
@@ -0,0 +1,9 @@
+color1 = input ("enter a color: ")
+color2 = input ("enter another color: ")
+noun1 = input ("enter a noun: ")
+exp = input ("enter your expression: ")
+
+print ("Roses are ",color1)
+print (color2, "are blue")
+print ("you are my ",noun1)
+print("and I",exp, "you")
