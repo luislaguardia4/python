ae4aa820845d256d1ebed5c991bdbefdc8a26c09
Luis Laguardia <61608109+luislaguardia4@users.noreply.github.com>
2023-03-21 01:01:25 +0800

    Create lightyear.py

---
+++ lightyear.py
@@ -0,0 +1,5 @@
+year = int(input("enter number of years: "))
+rate = int ( 3 * 10 ** 8)
+secondsinYear = int (365 * 24 *60 ** 2)
+distanceperSec = (secondsinYear * rate)
+print ("Distance per second is: " ,distanceperSec)
