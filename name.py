f4e47bf45b5f6bfede0a75f3c18acac160a6c5ab
Luis Laguardia <61608109+luislaguardia4@users.noreply.github.com>
2023-03-21 00:58:55 +0800

    Create name.py

---
+++ name.py
@@ -0,0 +1,15 @@
+#basics
+name = "Luis"
+age = 19
+
+print ("Hi!", name , ", are you" , age , "?")
+
+#2
+
+
+name = input ("what is your name? ")
+print (name)
+age = input ("how old are you? ")
+print (age)
+
+print ("Hi!", name, ", are you" , age ,"?")
